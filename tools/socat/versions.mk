b9633a05a3efed8c7429757babf8f28deac4490b
Colin McIntosh <colin@colinmcintosh.com>
2026-09-09 09:24:18 -0700

    Fetch socat from Debian HTTPS instead of dest-unreach.
    dest-unreach.org serves a self-signed certificate, so Alpine wget fails TLS verification. Debian's orig tarball is the same 1.8.1.3 release under a public CA.

--- tools/socat/versions.mk
+++ tools/socat/versions.mk
@@ -1,5 +1,7 @@
 include ../../deps/versions.mk
 
 SOCAT_VERSION := 1.8.1.3
-SOCAT_SOURCE_URL := https://www.dest-unreach.org/socat/download/socat-$(SOCAT_VERSION).tar.gz
-SOCAT_SOURCE_SHA256 := 06602ffd591e98c75b3dc1d66f0f19136cc666b0b2d95caad987d6ab2cb28097
+# dest-unreach.org HTTPS presents a self-signed cert, so fetch Debian's
+# orig tarball of the same upstream release over a public CA.
+SOCAT_SOURCE_URL := https://deb.debian.org/debian/pool/main/s/socat/socat_$(SOCAT_VERSION).orig.tar.bz2
+SOCAT_SOURCE_SHA256 := 25bc6476292b2e614220989c77b0b6fca87bb2525d9747b31a6639b1fb602418
